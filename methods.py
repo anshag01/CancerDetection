8b68faf9ec563fc56625e198af2c6ee65f1b352c
ansha <ansh.agrawal@mail.utoronto.ca>
2024-04-07 18:34:02 +0800

    ok

--- methods.py
+++ methods.py
@@ -16,24 +16,3 @@ def preprocess_image(image_path):
     resized_img = cv2.resize(gray_img, (600, 450))
     return resized_img
 
-
-# def build_model():
-#     model = Sequential([
-#         Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 1)),
-#         MaxPooling2D(2, 2),
-#         Conv2D(64, (3, 3), activation='relu'),
-#         MaxPooling2D(2, 2),
-#         Flatten(),
-#         Dense(128, activation='relu'),
-#         Dense(1, activation='sigmoid')  # Using sigmoid for binary classification
-#     ])
-#
-#     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-#     return model
-#
-# model = build_model()
-# model.fit(train_images, train_labels, epochs=10, validation_data=(validation_images, validation_labels))
-#
-# processed_img = preprocess_image('path_to_new_image.jpg')
-# prediction = model.predict(processed_img)
-# print("Cancer Detected" if prediction[0][0] > 0.5 else "No Cancer Detected")
