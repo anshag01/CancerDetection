b0fbb279fb04ced5c07d7b7959ba00746cc65f2b
AkhilChoraria <chorariaakhil03@gmail.com>
2024-04-12 18:58:27 +0800

    Worked on histogram of color distribution and contrast

--- methods.py
+++ methods.py
@@ -1,39 +1,52 @@
 import cv2
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-
-
-def preprocess_image(image_path):
+def convert_grayscale(image_path):
     # Load the image
     img = cv2.imread(image_path)
     # Convert to grayscale
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # print(gray_img.dtype)
-    # Resize the image to (600, 450)
-    resized_img = cv2.resize(gray_img, (600, 450))
-    return resized_img
+    #resized_img = cv2.resize(gray_img, (600, 450))
+    return gray_img
 
+def convert_rgb(image_path):
+    """
+    Converts the image to normalised RGB format
+    """
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    normalised_img = img / 255.0
+    return normalised_img
 
-# def build_model():
-#     model = Sequential([
-#         Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 1)),
-#         MaxPooling2D(2, 2),
-#         Conv2D(64, (3, 3), activation='relu'),
-#         MaxPooling2D(2, 2),
-#         Flatten(),
-#         Dense(128, activation='relu'),
-#         Dense(1, activation='sigmoid')  # Using sigmoid for binary classification
-#     ])
-#
-#     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-#     return model
-#
-# model = build_model()
-# model.fit(train_images, train_labels, epochs=10, validation_data=(validation_images, validation_labels))
-#
-# processed_img = preprocess_image('path_to_new_image.jpg')
-# prediction = model.predict(processed_img)
-# print("Cancer Detected" if prediction[0][0] > 0.5 else "No Cancer Detected")
+def color_distribution(img):
+    """
+    Returns the color distribution of the image
+    """
+    df = pd.DataFrame(img.reshape(-1, 3), columns=['R', 'G', 'B'])
+    color_distribution = df.describe()
+    return color_distribution
+
+def create_rgb_histogram(img):
+    """
+    Create one RGB histogram based on the distribution of the image
+    """
+    img = cv2.imread(img)
+    color = ('b', 'g', 'r')
+    for i, col in enumerate(color):
+        histr = cv2.calcHist([img], [i], None, [256], [0, 256])
+        plt.plot(histr, color=col)
+        plt.xlim([0, 256])
+    plt.show()
+    return histr
+    
+def get_contrast(img):
+    """
+    Returns the contrast of the image
+    """
+    img = convert_grayscale(img)
+    std_dev = np.std(img) #Contrast
+    return std_dev
+
