8c56bcbd218df14aeafaee8abc160d16d23c197a
Julian Santen <jsanten@ethz.ch>
2024-04-20 13:59:37 +0800

    added shap values, t-SNE, kernel PCA

--- methods_feature_extraction_model.py
+++ methods_feature_extraction_model.py
@@ -7,6 +7,13 @@ from torch.utils.data import Dataset
 
 import methods
 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+
+from sklearn import datasets
+from sklearn.manifold import TSNE
 
 class ImageDataset(Dataset):
     def __init__(self, directory, transform=None):
@@ -31,13 +38,6 @@ class ImageDataset(Dataset):
         key = os.path.basename(image_path).removesuffix(".jpg").removesuffix(".png")
         return key, image_tensor
 
-
-def list_image_files(data_folder_path: str) -> list:
-    """List the base filenames of JPG images in the specified directory."""
-    files = [f for f in os.listdir(data_folder_path) if f.endswith(".jpg")]
-    return [os.path.splitext(f)[0] for f in files]
-
-
 def extract_image_ids(filenames: list) -> pd.DataFrame:
     """Extract image IDs from filenames assuming format includes ID as the last two underscore-separated parts."""
     image_ids = ["_".join(name.split("_")[-2:]) for name in filenames]
@@ -53,23 +53,24 @@ def load_features(features_path: str) -> pd.DataFrame:
 
 
 def merge_features_with_labels(
-    data_folder_path: str,
     features_path: str,
     labels_df: pd.DataFrame,
     export: bool = False,
 ) -> pd.DataFrame:
     """Merge image features with labels into a single DataFrame."""
-    filenames = list_image_files(data_folder_path)
+    features = load_features(features_path)
+
+    filenames = features.T.columns.to_numpy()
     temp_files = extract_image_ids(filenames)
 
     label_data = temp_files.merge(labels_df, on="image_id", how="left")
-    features = load_features(features_path)
+
 
     merged_data = features.merge(label_data, left_index=True, right_on="image_id")
 
     if export:
         export_path = os.path.join(
-            os.path.dirname(features_path), "pandas_features.csv"
+            os.path.dirname(features_path), features_path.split(".")[-2].split("/")[-1] +  "_pandas.csv"
         )
         merged_data.to_csv(export_path)
 
@@ -115,3 +116,73 @@ def calculate_test_size(dataframe, test_size, include_in_testing):
     unique_image_count = len(np.unique(dataframe.image_id))
     valid_image_count = np.sum(include_in_testing)
     return test_size * unique_image_count / valid_image_count
+
+
+
+def calculate_metrics(y_test, y_pred):
+    """
+    Calculates and prints the accuracy, precision, recall, and F1 score for the given test labels and predictions.
+
+    :param y_test: True labels for the test set.
+    :param y_pred: Predicted labels for the test set.
+    :return:
+    """
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, pos_label=1)
+    recall = recall_score(y_test, y_pred, pos_label=1)
+    f1 = f1_score(y_test, y_pred, pos_label=1)
+
+    print(f"Accuracy: {accuracy}")
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    print(f"F1 Score: {f1}")
+
+
+
+
+def plot_confusion_matrix(y_test, y_pred, print_metrics=True):
+    """
+    Plots the confusion matrix for the given test labels and predictions.
+
+    :param y_test: True labels for the test set.
+    :param y_pred: Predicted labels for the test set.
+    :return:
+    """
+    if print_metrics:
+        calculate_metrics(y_test, y_pred)
+
+    # Compute the confusion matrix
+    conf_matrix = confusion_matrix(y_test, y_pred)
+
+    # Initialize the ConfusionMatrixDisplay object with the confusion matrix
+    cmd = ConfusionMatrixDisplay(conf_matrix)
+
+    # Plot the confusion matrix
+    cmd.plot(cmap=plt.cm.Blues)
+    plt.title("Confusion Matrix")
+    plt.show()
+
+def plot_low_dim_components(x_train_low_dim, y_train, label="PCA", component_1=0, component_2=1):
+    # Scatter plot of the first two PCA components
+    # Here, X_pca[:, 0] is the first component, X_pca[:, 1] is the second component
+    plt.figure(figsize=(10, 7))
+    plt.scatter(
+        x_train_low_dim[y_train == 0, component_1],
+        x_train_low_dim[y_train == 0, component_2],
+        c="blue",
+        label="Non-Cancerous",
+        alpha=0.5,
+    )  # Non-cancerous in blue
+    plt.scatter(
+        x_train_low_dim[y_train == 1, component_1],
+        x_train_low_dim[y_train == 1, component_2],
+        c="red",
+        label="Cancerous",
+        alpha=0.5,
+    )  # Cancerous labeled in red
+
+    # Adding labels and title
+    plt.xlabel(f"{label} Component {component_1}")
+    plt.ylabel(f"{label} Component {component_2}")
+    plt.title(f"{label} of Image Data")
+    plt.legend()
